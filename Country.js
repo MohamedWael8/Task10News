5b71ff9030adf181c73b497a035007025b25edad
Mohamed Wael <Mohamed.Wael@itworx.com>
2019-02-17 12:06:54 +0200

    Countries displayed, re-style country view, start news

--- Country.js
+++ Country.js
@@ -1,26 +1,27 @@
 
 var country = (function IIFE() 
 {
-    var model = 
+    var state = 
     {
-        data:{},
-        urlBase:"https://restcountries.eu/rest/v2",
-        urlExtra:"/alpha/",
-        urlCountry:"",
+        data:[],
+        urlCountryBase:"https://restcountries.eu/rest/v2",
+        urlAlphaExtra:"/alpha/",
+        urlRegionExtra:"/region/",
+        countryAlphaCode:"",
+        region:"",
         urlIP:"https://jsonip.com?callback=?",
-        ip:"",
+        userIP:"",
         urlCurrentCountry:"http://ip-api.com/json/"
     }
 
-    _cacheDom();
-    _eventListeners();
-    _render();
-    _search();
+    //cache dom
+    var template = $('#countryListTemplate').html();
+    var $target = $('#country-list');
 
-    function _cacheDom()
-    {
-        
-    }
+    _eventListeners();
+    _getIP();
+
+   
 
     function _eventListeners()
     {
@@ -29,22 +30,64 @@ var country = (function IIFE()
 
     function _render()
     {
-        
+        Mustache.parse(template);  
+        var rendered = Mustache.render(template, state);
+        $target.html(rendered);
     }
     
-    function _search()
+    function _getIP()
     {
-        $.ajax({
-            method: "GET",
-            url: model.urlBase + urlExtra + urlCountry
-          }).done(function(data) 
-          {
-            model.data = data;
-            console.log(model.data)
-          });
-        model.urlCountry = getISO3Country();
-        PublisherSubscriber.emit(_search,model.data);
-        _render();
+      console.log("started _getIP");
+      $.ajax({
+        method: "GET",
+        url: "https://jsonip.com?callback="
+      }).done(function(data) 
+      {
+        state.userIP = data.ip;
+        _getCurrentCountryAlpha();
+        console.log("finished _getIP");
+      });
+    }
+
+    function _getCurrentCountryAlpha()
+    {
+      $.ajax({
+        method: "GET",
+        url: "http://ip-api.com/json/"+state.userIP
+      }).done(function(data) 
+      {
+        console.log(state.userIP)
+        state.countryAlphaCode = data.countryCode;
+        _searchSingleCountryAlphaCode();
+        PublisherSubscriber.emit(_getCurrentCountryAlpha,state.countryAlphaCode);
+      });
+    }
+
+    function _searchSingleCountryAlphaCode()
+    {
+      console.log("start _searchSingleCountryAlphaCode")
+      $.ajax({
+        method: "GET",
+        url: state.urlCountryBase+state.urlAlphaExtra+state.countryAlphaCode
+      }).done(function(data) 
+      {
+        state.region = data.region;
+        PublisherSubscriber.emit(_searchSingleCountryAlphaCode,data.borders);
+        _searchRegion();
+      });
+    }
+
+    function _searchRegion()
+    {
+      $.ajax({
+          method: "GET",
+          url: state.urlCountryBase + state.urlRegionExtra + state.region
+        }).done(function(data) 
+        {
+          state.data = data;
+          _render();
+        });
+     
     }
 
     return
